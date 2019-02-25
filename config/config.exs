67fae6b85b752ce39dbcc10a75ae33a8cffe5f55
Alan Vardy <alanvardy@gmail.com>
2019-02-24 21:45:24 -0700

    wip

--- config/config.exs
+++ config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure your application as:
 #
-#     config :testing, key: :value
+#     config :elixir, key: :value
 #
 # and access this configuration in your application as:
 #
-#     Application.get_env(:testing, :key)
+#     Application.get_env(:elixir, :key)
 #
 # You can also configure a third-party app:
 #
--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule Testing.MixProject do
+defmodule Elixir.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :testing,
+      app: :elixir,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
